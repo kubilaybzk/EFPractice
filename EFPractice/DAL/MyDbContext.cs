3b8943180caf8f115e3157838f7960038610df0f
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2023-04-26 16:48:13 +0300

    Fluent Api.

--- EFPractice/DAL/MyDbContext.cs
+++ EFPractice/DAL/MyDbContext.cs
@@ -12,5 +12,14 @@ namespace EFPractice.DAL
         {
             optionsBuilder.UseSqlServer("Data Source=192.168.16.94;Initial Catalog=EFPractice;User ID=SA;Password=Password123;TrustServerCertificate=True");
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Bu şekilde constraint tanımlamaları fluent api ile yapılıyor . 
+
+            modelBuilder.Entity<Book>().HasKey(b => b.BookId); //Burada buranın primary key olduğunu belirtiyoruz.
+            
+            modelBuilder.Entity<Book>().Property(b => b.Title).IsRequired().HasMaxLength(150); //Burada required bir değer olduğunu ve maxsimum 150 karakter olmasını set ediyoruz.
+        }
     }
 }
--- EFPractice/Migrations/20230426131842_Fluent_Api.Designer.cs
+++ EFPractice/Migrations/20230426131842_Fluent_Api.Designer.cs
@@ -3,6 +3,7 @@ using EFPractice.DAL;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFPractice.Migrations
 {
     [DbContext(typeof(MyDbContext))]
-    partial class MyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230426131842_Fluent_Api")]
+    partial class Fluent_Api
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
