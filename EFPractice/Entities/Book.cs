3a1cff4ac70ffb1f3992be2b5c5f8890af79ea91
Kubilay Bozak <48950686+kubilaybzk@users.noreply.github.com>
2023-04-26 16:08:14 +0300

    Constraints. Net recommended.

--- EFPractice/Entities/Book.cs
+++ EFPractice/Entities/Book.cs
@@ -1,8 +1,14 @@
-﻿namespace EFPractice.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EFPractice.Entities
 {
     public class Book
     {
+        [Key] //Bu bu alanın bir primary kes olarak tanımlandığını ifade eder.
         public int BookId { get; set; }
+        [MaxLength(50)] //Maksimum 50 kelime olsun
+        [Required] // Sorunlu alan
+
         public string Title { get; set; }
     }
 }
